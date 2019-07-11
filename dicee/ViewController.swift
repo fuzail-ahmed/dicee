adb99efbc568775f681caf80ba154f16e5a49a00
Fuzail Ahmed <fuzail1280@gmail.com>
2019-07-11 18:48:41 +0530

    added functionality to dice

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var image_view_dice_1: UIImageView!
+    @IBOutlet weak var image_view_dice_2: UIImageView!
+    
+    var index_dice_1: Int = 0
+    var index_dice_2: Int = 0
+    let dice_arr: [String] = ["dice1", "dice2", "dice2", "dice3", "dice4", "dice5"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButtonPress(_ sender: Any) {
+        index_dice_1 = Int.random(in: 0 ... 5);
+        index_dice_2 = Int.random(in: 0 ... 5);
+        
+        image_view_dice_1.image = UIImage(named: dice_arr[index_dice_1])
+        image_view_dice_2.image = UIImage(named: dice_arr[index_dice_2])
+    }
+    
 }
 
