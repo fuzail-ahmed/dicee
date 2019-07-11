613683e93dba4d84c4b52312263fee25ce5b205e
Fuzail Ahmed <fuzail1280@gmail.com>
2019-07-11 18:02:28 +0530

    Project Setup 1. Project setup is done 2. Added assets in assets 3. Design with autolayout starts

---
+++ dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Fuzail Ahmed on 11/07/19.
+//  Copyright © 2019 Fuzail Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
